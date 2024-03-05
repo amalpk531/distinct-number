f1a0279a9cf19835a6f1a6787fa66c61c26091b9
amalpk531 <amal18120007@gmail.com>
2024-03-05 22:15:08 +0530

    p3

--- learning_2.py
+++ learning_2.py
@@ -31,4 +31,18 @@ def factorial(n):
 print(factorial(5))  
 
 # Output: 120
+#--------------------leap year-----------------------------------#
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+else:
+	if year % 4 != 0:
+		print("Common year")
+	elif year % 100 != 0:
+		print("Leap year")
+	elif year % 400 != 0:
+		print("Common year")
+	else:
+		print("Leap year")
 
