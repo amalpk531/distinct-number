e3e1a7bc8a125dde110da32f35eac8e16610f2ac
amal pk <132121679+amalpk531@users.noreply.github.com>
2024-03-19 17:16:30 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,32 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited ₹{amount:.2f}. Current balance: ₹{self.balance:.2f}.")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew ₹{amount:.2f}. Current balance: ₹{self.balance:.2f}.")
+        else:
+            print("Insufficient funds.")
+
+    def check_balance(self):
+        print(f"Account Balance: ₹{self.balance:.2f}.")
+
+# Creating an instance of BankAccount
+my_account = BankAccount("123456", 1000)
+
+# Taking user input for deposit amount
+deposit_amount = float(input("Enter the deposit amount in rupees: "))
+my_account.deposit(deposit_amount)
+
+# Taking user input for withdrawal amount
+withdrawal_amount = float(input("Enter the withdrawal amount in rupees: "))
+my_account.withdraw(withdrawal_amount)
+
+# Checking balance
+my_account.check_balance()
