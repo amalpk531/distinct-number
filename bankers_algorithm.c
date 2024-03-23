67ed978f0dbcb0fc555cfe35a723df6e14c8e649
amal pk <132121679+amalpk531@users.noreply.github.com>
2024-03-23 22:24:37 +0530

    Create bankers_algorithm

---
+++ bankers_algorithm.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+void main()
+{
+    int n, m, max[10][10], allo[10][10], avail[10], i, j, k, y;
+    printf("No of process");
+    scanf("%d", &n);
+    printf("no of resource");
+    scanf("%d", &m);
+    printf("max ");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+    printf("allocated");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &allo[i][j]);
+        }
+    }
+    printf("Available");
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &avail[i]);
+    }
+    int finish[n], safe[n], work[m], need[n][m], ind = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            need[i][j] = max[i][j] - allo[i][j];
+        }
+    }
+    printf("need matrix is:");
+    for (i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (j = 0; j < m; j++)
+        {
+            printf("%d\t", need[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        work[i] = avail[i];
+        finish[i] = 0;
+    }
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < n; i++)
+        {
+            if (finish[i] == 0)
+            {
+                int flag = 0;
+                for (j = 0; j < m; j++)
+                {
+                    if (need[i][j] > work[j])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    safe[ind++] = i;
+                    for (y = 0; y < m; y++)
+                    {
+                        work[y] += allo[i][y];
+                        finish[i] = 1;
+                    }
+                }
+            }
+        }
+    }
+    printf("\nsafe sequc");
+    for (i = 0; i <= n - 1; i++)
+    {
+        printf("p %d\t", safe[i]);
+    }
+}
