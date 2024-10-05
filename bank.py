4193e40ff614d4525aed3d16dff47eaa3cec8731
amal pk <132121679+amalpk531@users.noreply.github.com>
2024-10-05 21:49:13 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-##------------------------------bank account balance-------------------------
+###------------------------------bank account balance-------------------------
 class BankAccount:
     def __init__(self, account_number, balance):
         self.account_number = account_number
